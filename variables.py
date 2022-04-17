c5e0e4325d242e460b572a29868985bb32bafa62
tushar7247 <54477904+tushar7247@users.noreply.github.com>
2022-04-17 23:41:02 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,21 @@
+
+a = "Tushar"    
+b = 245           
+c = 34.78
+d = True
+e = None
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# printing the type of varaibles
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
